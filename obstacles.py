12a8569483a513360bb160bde74309751498116a
Anastasiia <afedorchenkoa@gmail.com>
2026-03-10 23:13:29 +0200

    Додано базовий клас Spawner для створення об'єктів

--- obstacles.py
+++ obstacles.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 class Obstacle:
     def __init__(self, x, y, speed, obs_type):
@@ -13,7 +14,7 @@ class Obstacle:
             # Машина: ширина 50, висота 80
             self.rect = pygame.Rect(x, y, 50, 80)
         else:
-            # Інше (наприклад, конус): ширина 30, висота 30
+            # Інше (конус): ширина 30, висота 30
             self.rect = pygame.Rect(x, y, 30, 30)
 
     def update(self, dt):
@@ -21,3 +22,16 @@ class Obstacle:
 
     def is_off_screen(self, screen_height):
         return self.rect.y > screen_height # Перевіряємо, чи перешкода вийшла за межі екрану (тобто, якщо її верхня межа (y) перевищує висоту екрану)
+    
+class Spawner:
+    def __init__(self):
+        self.obstacles = []
+        self.spawn_timer = 0
+
+    def spawn(self, screen_width):
+        #Створює одну перешкоду у випадковому місці
+        x = random.randint(100, screen_width - 100)
+        y = -100
+        speed = 250  # Поки що фіксована швидкість
+        new_obs = Obstacle(x, y, speed, "car")
+        self.obstacles.append(new_obs)
