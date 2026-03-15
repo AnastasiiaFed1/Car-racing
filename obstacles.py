0761c9bc176698be871712fc30d93ce44a882dba
Anastasiia <afedorchenkoa@gmail.com>
2026-03-15 22:47:49 +0200

    Додано методи draw для візуалізації перешкод

--- obstacles.py
+++ obstacles.py
@@ -23,6 +23,15 @@ class Obstacle:
     def is_off_screen(self, screen_height):
         return self.rect.y > screen_height # Перевіряємо, чи перешкода вийшла за межі екрану (тобто, якщо її верхня межа (y) перевищує висоту екрану)
     
+    def draw(self, surface):
+        if self.obs_type == "car":
+            color = (200, 0, 0)  # Червоний для машин
+        else:
+            color = (255, 165, 0)  # Помаранчевий для конусів
+            
+        pygame.draw.rect(surface, color, self.rect)
+    
+
 class Spawner:
     def __init__(self):
         self.obstacles = []
@@ -65,3 +74,7 @@ class Spawner:
             # 4. Якщо перешкода виїхала за екран — видаляємо її зі списку
             if obstacle.is_off_screen(screen_height):
                 self.obstacles.remove(obstacle)
+
+    def draw(self, surface):
+        for obstacle in self.obstacles:
+            obstacle.draw(surface)
