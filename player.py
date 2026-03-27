d91cb81153869c4ab5aca327baa297b48c2d0505
olya <bachyrinaolya94@gmail.com>
2026-03-27 23:10:32 +0200

    "Реалізація методу handle_input для руху по x та y "

--- player.py
+++ player.py
@@ -8,6 +8,23 @@ class PlayerCar():
         self.speed = speed
         self.hitbox = self.rect.inflate(-10, -10)
         self.image = pygame.Surface((50, 100))
+    
+    def handle_input(self):
+
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_LEFT]:
+            self.rect.x -= 10
+            print(self.rect)
+        if keys[pygame.K_RIGHT]:
+            self.rect.x += 10
+            print(self.rect)
+        if keys[pygame.K_UP]:
+            self.rect.y -= self.speed
+            print(self.rect)
+        if keys[pygame.K_DOWN]:
+            self.rect.y += self.speed
+            print(self.rect)
 
 player = PlayerCar(100, 200, 5)
 print(player.rect)  
