b4ce1617bbc9f2582a1a6224863616e31e376292
katyadyadenko <katerina.dyadenko@gmail.com>
2026-03-31 16:16:23 +0300

    Закоментувала

--- game.py
+++ game.py
@@ -3,10 +3,10 @@ import pygame
 from settings import SCREEN_W, SCREEN_H, FPS, BG_COLOR, TEXT_COLOR
 from state import GameState
 
-from player import PlayerCar
-from obstacles import ObstacleManager
-from road import Road
-from ui import UI
+# from player import PlayerCar
+# from obstacles import ObstacleManager
+# from road import Road
+# from ui import UI
 
 class Game:
     def __init__(self):
